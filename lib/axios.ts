e4b888e768e83bb850598e5a0ff3b156c22a51f7
abdullaalfahad <aafahad02@gmail.com>
2025-10-16 20:54:51 +0600

    chore: setup redux toolkit axios

---
+++ lib/axios.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { store } from "@/redux/store";
+
+const api = axios.create({
+  baseURL: "https://api.bitechx.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = store.getState().auth.token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
